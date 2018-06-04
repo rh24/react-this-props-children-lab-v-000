3894a4c154b5fbb056202bcd0d518ff9af3fa6eb
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-04 12:04:16 -0400

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,15 @@
-// Code Invitation Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Invitation extends React.Component {
+  render() {
+    return (
+      <div className="invitation">
+        <h1>You've been invited!</h1>
+        <div className="invitation-children">{this.props.children}</div>
+      </div>
+    );
+  }
+}
+
+export default Invitation;
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,14 @@
-// Code ThemedDecoration Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ThemedDecorations extends React.Component {
+  render() {
+    return (
+      <div className="themed-decorations">
+        {this.props.children.map(child => <div className="heaven">{child}</div>)}
+      </div>
+    );
+  }
+}
+
+export default ThemedDecorations;
